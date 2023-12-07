70dc6c84e2475aba4f1cbeacbc6a812b4d9a3411
Matthias Kuhn <matthias@opengis.ch>
2023-12-07 13:39:45 +0100

    pyqt5

---
+++ vcpkg/overlay/python-base-packages/portfile.cmake
@@ -0,0 +1,22 @@
+set(PYTHON3 "${CURRENT_INSTALLED_DIR}/tools/python3/python3.11")
+
+set(WORKING_DIR "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/")
+execute_process(
+	COMMAND mkdir -p "${WORKING_DIR}/"
+)
+
+message(STATUS "Running pip install...")
+# TODO: this is a bad idea as it installs directly into the install directory
+message(STATUS "Bootstrap pip...")
+vcpkg_execute_required_process(
+	COMMAND "${PYTHON3}" "-m" "ensurepip"
+	WORKING_DIRECTORY "${WORKING_DIR}"
+        LOGNAME "ensurepip-${TARGET_TRIPLET}"
+)
+
+message(STATUS "Running setup install...")
+vcpkg_execute_required_process(
+	COMMAND "${PYTHON3}" "-m" "pip" "install" "-r" "${CURRENT_PORT_DIR}/requirements.txt" "-t" "${CURRENT_PACKAGES_DIR}/lib/python3.11/site-packages"
+	WORKING_DIRECTORY "${WORKING_DIR}"
+        LOGNAME "setup-py-install-${TARGET_TRIPLET}"
+)
