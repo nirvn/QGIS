39db7c085292fa72201213abe24a8892ad4b6cdc
Matthias Kuhn <matthias@opengis.ch>
2023-12-25 10:10:02 +0100

    Ensurepip into package

--- vcpkg/overlay/python-base-packages/portfile.cmake
+++ vcpkg/overlay/python-base-packages/portfile.cmake
@@ -9,7 +9,7 @@ message(STATUS "Running pip install...")
 # TODO: this is a bad idea as it installs directly into the install directory
 message(STATUS "Bootstrap pip...")
 vcpkg_execute_required_process(
-	COMMAND "${PYTHON3}" "-m" "ensurepip"
+	COMMAND "${PYTHON3}" "-m" "ensurepip" "--root" "${CURRENT_PACKAGES_DIR}/lib/python3.11/site-packages"
 	WORKING_DIRECTORY "${WORKING_DIR}"
         LOGNAME "ensurepip-${TARGET_TRIPLET}"
 )
